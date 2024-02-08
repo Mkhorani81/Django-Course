af4eb8986118a04b60c391984ab8042ea6e7ea83
unknown <7m.khorani81@gmail.com>
2024-02-08 16:52:01 +0330

    put code on github

---
+++ blog/views.py
@@ -0,0 +1,56 @@
+import username
+from django.shortcuts import render, get_object_or_404
+from account.models import UserInfo
+from django.views.generic import ListView, DetailView
+from .models import Article, Categoty
+from account.mixins import AuthorAccessMixin
+
+# Create your views here.
+
+class ArticleList(ListView):
+    queryset = Article.objects.published()
+    paginate_by = 6
+
+
+class ArticleDetail(DetailView):
+    def get_object(self):
+        slug = self.kwargs.get('slug')
+        return get_object_or_404(Article.objects.published(), slug=slug)
+
+
+class ArticlePreview(AuthorAccessMixin,DetailView):
+    def get_object(self):
+        pk = self.kwargs.get('pk')
+        return get_object_or_404(Article, pk=pk)
+
+
+class CategoryList(ListView):
+    paginate_by = 5
+    template_name = 'blog/category_list.html'
+
+    def get_queryset(self):
+        global category
+        slug = self.kwargs.get('slug')
+        category = get_object_or_404(Categoty.objects.active(), slug=slug)
+        return category.articles.published()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category'] = category
+        return context
+
+
+class AuthorList(ListView):
+    paginate_by = 5
+    template_name = 'blog/author_list.html'
+
+    def get_queryset(self):
+        global author
+        username = self.kwargs.get('username')
+        author = get_object_or_404(UserInfo, username=username)
+        return author.articles.published()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['author'] = author
+        return context
